cbd99f33c59c16333ba5242a50eae7c1be9ce3f0
Артём Гартман <chopktv@gmail.com>
2020-02-19 22:51:20 +0700

    Connection String added (UseSqlServer)

--- MovieCollection.UI/Startup.cs
+++ MovieCollection.UI/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieCollection.Database;
 
 namespace MovieCollection.UI
 {
@@ -24,6 +26,8 @@ namespace MovieCollection.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
