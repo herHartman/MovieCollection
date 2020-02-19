1e63dc064a1e96216a096a7b142b56cce3f87bd4
Артём Гартман <chopktv@gmail.com>
2020-02-19 22:52:44 +0700

    fix

--- MovieCollection.Models/Country.cs
+++ MovieCollection.Models/Country.cs
@@ -9,5 +9,6 @@ namespace MovieCollection.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<MovieCountry> MovieCountries { get; set; }
+        public ICollection<Premiere> Premieres { get; set; }
     }
 }
