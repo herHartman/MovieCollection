05c368fb54c272f7e8716fb3bfb47d66867fcd1e
Артём Гартман <chopktv@gmail.com>
2020-02-19 22:52:29 +0700

    Fix

--- MovieCollection.Models/Movie.cs
+++ MovieCollection.Models/Movie.cs
@@ -13,12 +13,14 @@ namespace MovieCollection.Models
         public string Year { get; set; }
         public decimal Budget { get; set; }
         public decimal Dues { get; set; }
-        public string WorldPremiere { get; set; }
-        public string DvdRealiase { get; set; }
-        public string BluRayRealise { get; set; }
+        public int Time { get; set; }
+        public DateTime DvdRealiase { get; set; }
+        public DateTime BluRayRealise { get; set; }
         public int Rating { get; set; }
+        public int PG { get; set; }
         public ICollection<MoviePeople> MoviePeoples { get; set; }
         public ICollection<MovieGenre> MovieGenres { get; set; }
         public ICollection<MovieCountry> MovieCountries { get; set; }
+        public ICollection<Premiere> Premieres { get; set; }
     }
 }
